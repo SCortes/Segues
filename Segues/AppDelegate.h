6e1ee3608840107371c1751b9f8d5a81935e6102
Sergio <sergio@MacBook-Pro-de-Sara-Dominguez.local>
2013-11-16 18:53:01 +0100

    Initial Commit

---
+++ Segues/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Segues
+//
+//  Created by Sergio on 16/11/13.
+//  Copyright (c) 2013 Sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SeguesTests/SeguesTests.m
@@ -0,0 +1,34 @@
+//
+//  SeguesTests.m
+//  SeguesTests
+//
+//  Created by Sergio on 16/11/13.
+//  Copyright (c) 2013 Sergio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SeguesTests : XCTestCase
+
+@end
+
+@implementation SeguesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
