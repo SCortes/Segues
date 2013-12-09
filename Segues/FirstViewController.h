d41109e407dd2ab5557fc7b2156446ed090d687d
Sergio <sergio@MacBook-Pro-de-Sara-Dominguez.local>
2013-12-09 23:17:24 +0100

    Inicio de Repositorio

---
+++ Segues/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Segues
+//
+//  Created by Sergio on 17/11/13.
+//  Copyright (c) 2013 Sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+
+@end
---
+++ Segues/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Segues
+//
+//  Created by Sergio on 17/11/13.
+//  Copyright (c) 2013 Sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
